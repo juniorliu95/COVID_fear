eb514c1e327a412e2dd1e415041c437a4b01c73b
junyu liu <juniorliu95@gmail.com>
2022-05-16 21:32:35 +0900

    revise select_keywords

--- 1_3_select_keywords.py
+++ 1_3_select_keywords.py
@@ -5,8 +5,6 @@ import shutil
 import codecs
 
 title = 'vaccine'
-# title = "olympics"
-# title = 'vaccine_and_olympics'
 
 root_dir = os.getcwd()
 result_root_dir = os.path.join(root_dir, 'results', title)
@@ -14,8 +12,8 @@ if not os.path.exists(result_root_dir):
     os.makedirs(result_root_dir)
 
 # dir of the texts to be cleaned.
-start_date = '2021-07-01'
-end_date = None
+start_date = '2021-02-01'
+end_date = '2021-10-01'
 data_dir = os.path.join(root_dir, 'results', 'text')
 all_date_list = list(sorted(os.listdir(data_dir)))
 
@@ -31,10 +29,7 @@ if start_date:
             all_date_list_temp.append(date)
     all_date_list = all_date_list_temp
 
-# ! also change the keyword
-keywords = []
-keywords += ['ワクチン','接種','せっしゅ','注射','ちゅうしゃ','打つ', '投与', 'とうよ','mRNA', '副作用', 'ふくさよう', '副反応','ふくはんのう', 'vaccine','vacc', 'pfizer','ファイザー', 'astra','zeneca','アストラ','ゼネカ', 'modelna','moderna', 'モデルナ']
-# keywords += ['五輪', 'ごりん', 'オリンピック', '選手', 'せんしゅ', 'olympic', 'tokyo 2020', '東京2020', 'ときょう2020', 'メダル', '開催式', '決勝']
+keywords = ['ワクチン','接種','せっしゅ','注射','ちゅうしゃ','打つ', '投与', 'とうよ','mRNA', '副作用', 'ふくさよう', '副反応','ふくはんのう', 'vaccine','vacc', 'pfizer','ファイザー', 'astra','zeneca','アストラ','ゼネカ', 'modelna','moderna', 'モデルナ']
 
 for d_ind, date in enumerate(all_date_list):
     # judge if the date is within the selected date range
@@ -48,7 +43,7 @@ for d_ind, date in enumerate(all_date_list):
     for tweet_file in os.listdir(os.path.join(data_dir, date)):
         with codecs.open(os.path.join(data_dir, date, tweet_file), 'r', 'utf-8') as f:
             text = f.readline().strip()
-            if "まとまっ" in text and "スマート" in text and "ニュース" in text:
+            if "まとまっ" in text and "スマート" in text and "ニュース" in text: # reject advertisements
                 continue
             
             for keyword in keywords:
